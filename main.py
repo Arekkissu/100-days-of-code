8d84ac3cc75ef71c64bdae6d356dfc386aaab262
Alex Rodrigues dos Santos <59795920+Arekkissu@users.noreply.github.com>
2022-02-22 01:09:24 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+numero_1 = int(two_digit_number[0])
+numero_2 = int(two_digit_number[1])
+print(numero_1 + numero_2)
+
+
+
+
+
+
